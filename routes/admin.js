eb94e67c0698d51e2a722b0ab531b4b04c85d73f
Ahad <115078116+alwaysahad@users.noreply.github.com>
2024-11-30 21:51:54 +0530

    updated admin code

--- routes/admin.js
+++ routes/admin.js
@@ -1,32 +1,59 @@
 const { Router } = require("express");
 const adminRouter = Router();
 const { adminModel } = require("../db");
+const { z } = require("zod");
 
-adminRouter.post("/signup", function(req, res) {
+adminRouter.post("/signup", function (req, res) {
+  const requiredBody = z.object({
+    email: z.string().email().min(6).max(50),
+    password: z.string().min(4).max(50),
+    firstName: z.string(),
+    lastName: z.string(),
+  });
+
+  const parsedDataWithSuccess = requiredBody.safeParse(req.body);
+
+  if (!parsedDataWithSuccess.success) {
+    res.json({
+      msg: "something went wrong",
+      err: parsedDataWithSuccess.error,
+    });
+    return;
+  }
+
+  const email = req.body.email;
+  const password = req.body.password;
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastname;
+
   res.json({
     msg: "signup endpoint",
   });
 });
 
-adminRouter.post("/signin", function(req, res) {
+adminRouter.post("/signin", function (req, res) {
+  const requiredBody = z.object({
+    email: z.string(),
+    password: z.string(),
+  });
   res.json({
     msg: "signin endpoint",
   });
 });
 
-adminRouter.get("/bulk", function(req, res) {
+adminRouter.get("/course/bulk", function (req, res) {
   res.json({
     msg: "bulk endpoint",
   });
 });
 
-adminRouter.put("/", function(req, res) {
+adminRouter.put("/course", function (req, res) {
   res.json({
     msg: "course endpoint",
   });
 });
 
-adminRouter.post("/", function(req, res) {
+adminRouter.post("/course", function (req, res) {
   res.json({
     msg: "course endpoint",
   });
